1512453af071072f46b3f0877f1fe697caa47331
BosEriko <bos.eriko@gmail.com>
2019-12-29 17:08:47 +0800

    add firebase config

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,19 @@ import Discord from 'discord.js'
 
 const client = new Discord.Client()
 
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "glootie-bot.firebaseapp.com",
+  databaseURL: "https://glootie-bot.firebaseio.com",
+  projectId: "glootie-bot",
+  storageBucket: "glootie-bot.appspot.com",
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+}
+
+firebase.initializeApp(firebaseConfig)
+
 client.once('ready', () => {
   console.log('Ready!')
 })
@@ -13,4 +26,4 @@ client.on('message', (message:any) => {
   }
 })
 
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.DISCORD_BOT_TOKEN)
