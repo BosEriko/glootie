c5967a6658650c131cf4746de6e22c65e7baf5cc
BosEriko <bos.eriko@gmail.com>
2019-12-29 14:24:26 +0800

    print process.env for testing

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,8 @@ const client = new Discord.Client();
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
 client.once('ready', () => {
-	console.log('Ready!');
+  console.log('Ready!');
+  console.log(process.env)
 });
 
 client.on('message', (message:any) => {
